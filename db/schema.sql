e2c6d011811a164e10a5d9c651b84561d3be4c2d
John Coleman <johncoleman_2@hotmail.com>
2018-04-27 23:37:07 -0500

    another push

--- db/schema.sql
+++ db/schema.sql
@@ -7,5 +7,6 @@ CREATE TABLE burgers (
     id int AUTO_INCREMENT,
     burger_name varchar(255) NOT NULL,
     devoured BOOLEAN DEFAULT false NOT NULL,
+    reordered BOOLEAN DEFAULT false NOT NULL,
   PRIMARY KEY(id)
 );
